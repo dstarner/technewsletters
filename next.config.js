29432bd275374a715ada7f985f88bc4e6708ef8d
Dan Starner <starner.daniel5@gmail.com>
2021-12-22 14:49:15 -0500

    getting some actual structure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'cdn.substack.com',
+      'buttondown.s3.amazonaws.com'
+    ],
+  },
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,31 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
+
+const newsletters = [
+  {
+    title: "Read Max",
+    imgSrc: "https://cdn.substack.com/image/fetch/w_96,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F5dccbdd7-40ff-4524-b1b3-862933aa12be_1280x1280.png", 
+    href: "https://maxread.substack.com/",
+    subscribeHref: "https://maxread.substack.com/subscribe",
+    description: `
+Read Max is a 2x-a-week newsletter by Max Read (that's me) about the future.
+    `,
+    tags: ["future", "technology"]
+  },
+  {
+    title: "rendezvous with cassidoo",
+    imgSrc: "https://buttondown.s3.amazonaws.com/icons/cfaf57f2-7aba-4c14-b56b-d41008562067.png", 
+    href: "https://buttondown.email/cassidoo/",
+    description: `
+    In this weekly newsletter I'm going to share with you what's new and cool in the world of web development, with content for everyone, from beginners to pros. There's also an interview question of the week, where you can test your skills and I'll share the results the following week.
+    I'll also send you jokes and stories.
+    And pics of mechanical keyboards.
+    I won't spam though, I promise.
+    `,
+    tags: ["web", "keyboards", "programming"]
+  }
+];
 
 export default function Home() {
   return (
@@ -11,7 +37,7 @@ export default function Home() {
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-4">
           <Link href="/">
             <a className="pl-4 flex items-center no-underline hover:no-underline" >
-              <img className="h-5 pr-3 fill-current text-purple-500" src="/logo.svg" />
+              <img className="h-5 pr-3 fill-current" src="/logo.svg" />
               <span className="text-gray-900 text-base font-extrabold text-xl">
                 Tech Newsletters
               </span>
@@ -31,8 +57,8 @@ export default function Home() {
           </div>
         </div>
       </nav>
-      <div className="container w-full flex flex-wrap mx-auto px-2 pt-8 lg:pt-16 mt-16">
-
+      <div className="container w-full flex flex-wrap mx-auto px-2 pt-16 lg:pt-10 mt-16 pb-16">
+        <NewsletterList newsletters={newsletters} />
       </div>
       <footer className="bg-white border-t border-gray-200 shadow">
         <div className="container mx-auto flex py-8">
@@ -41,7 +67,7 @@ export default function Home() {
               <div className="px-8">
                 <h3 className="font-bold text-gray-900">About</h3>
                 <p className="py-4 text-gray-600 text-sm">
-                  <FooterAboutLink href="/">Technewsletters.wiki</FooterAboutLink> was a fun side project
+                  <FooterAboutLink href="/">Technewsletters.wiki</FooterAboutLink> was a small side project
                   by <FooterAboutLink href="https://danstarner.com">Dan Starner</FooterAboutLink> when he
                   realized that it was difficult to find credible and popular newsletters to subscribe to
                   regarding technology and software engineering.
@@ -66,6 +92,59 @@ export default function Home() {
       </footer>
     </>
   )
+}
+
+function NewsletterList({ newsletters }) {
+  return (
+    <div aria-label="group of cards" tabIndex="0" className="focus:outline-none w-full">
+      <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-4">
+        {newsletters.map((newsletter, idx) => (
+          <NewsletterCard key={idx} {...newsletter} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function NewsletterCard({ title, href, subscribeHref, tags, description, imgSrc }) {
+  return (
+    <div aria-label="card 5" tabIndex="0" className="flex flex-col focus:outline-none w-full lg:mr-7 lg:mb-0 mb-7 bg-white p-6 shadow rounded">
+      <div className="flex items-center border-b border-gray-200 pb-6">
+        {imgSrc ? (
+          <Image className="rounded-full" src={imgSrc} width={50} height={50} alt={`${title} logo`} />
+        ) : (
+          <div className="w-12 h-12 bg-gray-300 rounded-full flex flex-shrink-0"></div>
+        )}
+        <div className="flex items-start justify-between w-full">
+          <div className="pl-3 w-full">
+            <p tabIndex="0" className="focus:outline-none text-xl font-medium leading-5 text-gray-800">{title}</p>
+            <a
+              target="_blank" rel="noreferrer" href={`${href}?utm_source=technewsletters.wiki`}
+              className="text-green-500 no-underline hover:text-green-700 hover:underline text-sm pt-2">
+                {href}
+              </a>
+          </div>
+          {subscribeHref && (
+            <>
+              <a target="_blank" rel="noreferrer" href={`${subscribeHref}?utm_source=technewsletters.wiki`} aria-label="subscribe">
+                <svg className="focus:outline-none hover:fill-green-300" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10.5001 4.66667H17.5001C18.1189 4.66667 18.7124 4.9125 19.15 5.35009C19.5876 5.78767 19.8334 6.38117 19.8334 7V23.3333L14.0001 19.8333L8.16675 23.3333V7C8.16675 6.38117 8.41258 5.78767 8.85017 5.35009C9.28775 4.9125 9.88124 4.66667 10.5001 4.66667Z" stroke="#2C3E50" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+      <div className="grow flex flex-col justify-between px-2">
+        <p tabIndex="0" className="focus:outline-none text-sm leading-5 py-4 text-gray-600">{description}</p>
+        <div tabIndex="0" className="focus:outline-none flex">
+          {tags.map((tag, idx) => (
+            <div key={tag} className={`${idx === 0 ? '' : 'ml-2'} py-2 px-4 text-xs leading-3 text-green-700 rounded-full bg-green-100`}>#{tag}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function FooterAboutLink({ children, href }) {
